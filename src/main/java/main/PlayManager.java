68c00e76acfaf0676da47cf221f0cc041b7a142f
IanLulu <ilulu2@uic.edu>
2023-10-16 14:05:08 -0500

    Implement game loop and gameplay manager class

---
+++ src/main/java/main/PlayManager.java
@@ -0,0 +1,17 @@
+package main;
+
+/*
+    Draws the play area
+    Manages tetrominoes
+    Handles gameplay actions (deleting lines, adding scores, etc.)
+ */
+public class PlayManager {
+
+    // Main play area
+    final int WIDTH = 360;
+    final int HEIGHT = 600;
+    public static int left_x;
+    public static int right_x;
+    public static int top_y;
+    public static int bottom_y;
+}
